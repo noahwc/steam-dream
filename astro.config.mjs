f33c2a8064d2a3a9ba6c6ddad7dc4f6f6b711ef2
Noah Clarke <noahwclarke@gmail.com>
2023-05-21 23:02:10 -0700

    load blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sanity({
-      projectId: "steam-dream",
+      projectId: "evu2rsa8",
       dataset: "production",
       apiVersion: "2023-02-25",
       useCdn: true,
